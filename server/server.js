2d98ad2c82c389b01c61824e31f1a70b0e96def8
MaybeItsAdam <100949524+MaybeItsAdam@users.noreply.github.com>
2022-06-28 12:56:24 +0100

    boilerplate server

--- server/server.js
+++ server/server.js
@@ -2,12 +2,12 @@ const http = require('http');
 const express = require('express');
 const app = express();
 
-app.use(express.static('${__dirname}/../client'));
+app.use(express.static(__dirname + '/../client'));
 
 const server = http.createServer(app);
 
 server.on('error', (err) => {
-  const.error(err);
+  console.error(err);
 });
 
 server.listen(8080, () => {

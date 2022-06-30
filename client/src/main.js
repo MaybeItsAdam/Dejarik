e24375dd81d9279007f835c87e4b342d3ae95748
MaybeItsAdam <100949524+MaybeItsAdam@users.noreply.github.com>
2022-06-30 17:23:11 +0100

    canvas positioned and players set

--- client/src/main.js
+++ client/src/main.js
@@ -1,14 +1,18 @@
+window.onload = function () {
+  var r_players = [];
+  var b_players = [];
 
-var r_players;
-var b_players;
+  var canvas = document.getElementById("pcs")
+  var pcs_context = canvas.getContext("2d");
 
-// load red players
-for (r=0; r<4; r++) {
-  // r_players[r]
-}
+  // load red players
+  for (r=0; r<4; r++) {
+    r_players[r] = {}
+  }
 
 
-// load blue players
-for (b=0; b<4; r++) {
-  // b_players[b]
+  // load blue players
+  for (b=0; b<4; r++) {
+    b_players[b] = {}
+  }
 }
